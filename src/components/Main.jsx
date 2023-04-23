0a252ccddd6cc4e1d9b58229740c557afc9d5ece
Samson Tush <samo4421@icloud.com>
2023-04-23 16:24:50 +0300

    Add some styling

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -12,71 +12,73 @@ const Main = () => (
         <h3 className="category">Action</h3>
         <div className="card-movie">
           <div className="card">
-            <img src="https://image.tmdb.org/t/p/w500/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg" alt="movie" />
+            <div className="image-div">
+              <img src="https://image.tmdb.org/t/p/w500/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg" alt="movie" />
+            </div>
             <p>Avatar</p>
           </div>
           <div className="card">
-            <img src="https://image.tmdb.org/t/p/w500/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg" alt="movie" />
+            <div className="image-div">
+              <img src="https://image.tmdb.org/t/p/w500/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg" alt="movie" />
+            </div>
             <p>Avatar</p>
           </div>
           <div className="card">
-            <img src="https://image.tmdb.org/t/p/w500/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg" alt="movie" />
-            <p>Avatar</p>
-          </div>
-        </div>
-      </div>
-      <div className="card-container">
-        <h3 className="category">Action</h3>
-        <div className="card-movie">
-          <div className="card">
-            <img src="https://image.tmdb.org/t/p/w500/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg" alt="movie" />
-            <p>Avatar</p>
-          </div>
-          <div className="card">
-            <img src="https://image.tmdb.org/t/p/w500/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg" alt="movie" />
-            <p>Avatar</p>
-          </div>
-          <div className="card">
-            <img src="https://image.tmdb.org/t/p/w500/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg" alt="movie" />
-            <p>Avatar</p>
-          </div>
-        </div>
-      </div>
-      <div className="card-container">
-        <h3 className="category">Action</h3>
-        <div className="card-movie">
-          <div className="card">
-            <img src="https://image.tmdb.org/t/p/w500/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg" alt="movie" />
-            <p>Avatar</p>
-          </div>
-          <div className="card">
-            <img src="https://image.tmdb.org/t/p/w500/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg" alt="movie" />
-            <p>Avatar</p>
-          </div>
-          <div className="card">
-            <img src="https://image.tmdb.org/t/p/w500/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg" alt="movie" />
-            <p>Avatar</p>
-          </div>
-        </div>
-      </div>
-      <div className="card-container">
-        <h3 className="category">Action</h3>
-        <div className="card-movie">
-          <div className="card">
-            <img src="https://image.tmdb.org/t/p/w500/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg" alt="movie" />
-            <p>Avatar</p>
-          </div>
-          <div className="card">
-            <img src="https://image.tmdb.org/t/p/w500/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg" alt="movie" />
-            <p>Avatar</p>
-          </div>
-          <div className="card">
-            <img src="https://image.tmdb.org/t/p/w500/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg" alt="movie" />
+            <div className="image-div">
+              <img src="https://image.tmdb.org/t/p/w500/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg" alt="movie" />
+            </div>
             <p>Avatar</p>
           </div>
         </div>
       </div>
 
+      <div className="card-container">
+        <h3 className="category">Action</h3>
+        <div className="card-movie">
+          <div className="card">
+            <div className="image-div">
+              <img src="https://image.tmdb.org/t/p/w500/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg" alt="movie" />
+            </div>
+            <p>Avatar</p>
+          </div>
+          <div className="card">
+            <div className="image-div">
+              <img src="https://image.tmdb.org/t/p/w500/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg" alt="movie" />
+            </div>
+            <p>Avatar</p>
+          </div>
+          <div className="card">
+            <div className="image-div">
+              <img src="https://image.tmdb.org/t/p/w500/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg" alt="movie" />
+            </div>
+            <p>Avatar</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="card-container">
+        <h3 className="category">Action</h3>
+        <div className="card-movie">
+          <div className="card">
+            <div className="image-div">
+              <img src="https://image.tmdb.org/t/p/w500/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg" alt="movie" />
+            </div>
+            <p>Avatar</p>
+          </div>
+          <div className="card">
+            <div className="image-div">
+              <img src="https://image.tmdb.org/t/p/w500/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg" alt="movie" />
+            </div>
+            <p>Avatar</p>
+          </div>
+          <div className="card">
+            <div className="image-div">
+              <img src="https://image.tmdb.org/t/p/w500/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg" alt="movie" />
+            </div>
+            <p>Avatar</p>
+          </div>
+        </div>
+      </div>
     </div>
   </main>
 );
