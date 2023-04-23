9deadbaa31beac09a0d6a70e89151634114ca815
byiringiroscar <byiringoroscar@gmail.com>
2023-04-23 14:24:36 +0200

    Add basic layout for main section

---
+++ src/components/Main.jsx
@@ -0,0 +1,84 @@
+import './main.scss';
+
+const Main = () => (
+  <main>
+    <h2 className="main-title">Movies</h2>
+    <p className="main-description">
+      Movies move us like nothing else can, whether they’re scary,
+      funny, dramatic, romantic or anywhere in-between. So many titles, so much to experience.
+    </p>
+    <div className="main-card-container">
+      <div className="card-container">
+        <h3 className="category">Action</h3>
+        <div className="card-movie">
+          <div className="card">
+            <img src="https://image.tmdb.org/t/p/w500/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg" alt="movie" />
+            <p>Avatar</p>
+          </div>
+          <div className="card">
+            <img src="https://image.tmdb.org/t/p/w500/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg" alt="movie" />
+            <p>Avatar</p>
+          </div>
+          <div className="card">
+            <img src="https://image.tmdb.org/t/p/w500/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg" alt="movie" />
+            <p>Avatar</p>
+          </div>
+        </div>
+      </div>
+      <div className="card-container">
+        <h3 className="category">Action</h3>
+        <div className="card-movie">
+          <div className="card">
+            <img src="https://image.tmdb.org/t/p/w500/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg" alt="movie" />
+            <p>Avatar</p>
+          </div>
+          <div className="card">
+            <img src="https://image.tmdb.org/t/p/w500/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg" alt="movie" />
+            <p>Avatar</p>
+          </div>
+          <div className="card">
+            <img src="https://image.tmdb.org/t/p/w500/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg" alt="movie" />
+            <p>Avatar</p>
+          </div>
+        </div>
+      </div>
+      <div className="card-container">
+        <h3 className="category">Action</h3>
+        <div className="card-movie">
+          <div className="card">
+            <img src="https://image.tmdb.org/t/p/w500/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg" alt="movie" />
+            <p>Avatar</p>
+          </div>
+          <div className="card">
+            <img src="https://image.tmdb.org/t/p/w500/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg" alt="movie" />
+            <p>Avatar</p>
+          </div>
+          <div className="card">
+            <img src="https://image.tmdb.org/t/p/w500/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg" alt="movie" />
+            <p>Avatar</p>
+          </div>
+        </div>
+      </div>
+      <div className="card-container">
+        <h3 className="category">Action</h3>
+        <div className="card-movie">
+          <div className="card">
+            <img src="https://image.tmdb.org/t/p/w500/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg" alt="movie" />
+            <p>Avatar</p>
+          </div>
+          <div className="card">
+            <img src="https://image.tmdb.org/t/p/w500/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg" alt="movie" />
+            <p>Avatar</p>
+          </div>
+          <div className="card">
+            <img src="https://image.tmdb.org/t/p/w500/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg" alt="movie" />
+            <p>Avatar</p>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </main>
+);
+
+export default Main;
